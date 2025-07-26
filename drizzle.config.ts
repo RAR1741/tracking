30a9a6db5e1a6853392275d35eaea7663aa2fb35
dracco1993 <dracco1993@gmail.com>
2025-07-26 02:54:03 +0000

    More logging

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,6 +4,9 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is required");
 }
 
+console.log("Using DATABASE_URL:", process.env.DATABASE_URL);
+console.log("Using NODE_ENV:", process.env.NODE_ENV);
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./database/schema.ts",
