2a96bd13a6b0543bec7e4d5ebcc90b1ca19a8232
dracco1993 <dracco1993@gmail.com>
2025-07-26 02:25:05 +0000

    Attempt to fix PG ssl error in prod

--- server/app.ts
+++ server/app.ts
@@ -17,7 +17,9 @@ export const app = express();
 
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is required");
 
-const client = postgres(process.env.DATABASE_URL);
+const client = postgres(process.env.DATABASE_URL, {
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
+});
 const db = drizzle(client, { schema });
 app.use((_, __, next) => DatabaseContext.run(db, next));
 
